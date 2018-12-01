1c63915c9f1afd6eeb949b25df4c0a5df0b893bc
henrywalton <henry.james.walton@gmail.com>
2018-12-01 11:48:35 +0000

    added ropeswing first draft

--- main.lua
+++ main.lua
@@ -1,16 +1,23 @@
 require("physics")
+require("ropeswing")
 
 function love.load()
     windowWidth = 1920
     windowHeight = 1080
     love.window.setMode(windowWidth, windowHeight)
     love.window.setFullscreen(true)
-    initialiseVariables()
+    initialisePhysicsVariables()
+    initialiseGlobalVariables()
     loadGraphics()
 end
 
 function love.update(dt)
     gravity()
+
+    --If left mouse button is pressed, try to rope swing
+    if love.mouse.isDown(1) then
+        shootRope()
+    end
 end
 
 function love.draw()
@@ -18,6 +25,10 @@ function love.draw()
     if down then
 	    love.graphics.print("Hello World!", 400, 300)
     end
+    if readyToDraw then
+        drawRope()
+    end
+
 end
 
 function loadGraphics()
@@ -28,4 +39,8 @@ function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
+end
+
+function initialiseGlobalVariables()
+    readyToDraw = false
 end
