d02c5a1b40bb74a0aa24a3fea48df3c6702d7599
henrywalton <henry.james.walton@gmail.com>
2018-12-01 14:16:29 +0000

    change contraction control

--- main.lua
+++ main.lua
@@ -21,10 +21,30 @@ function love.update(dt)
 
     world:update(dt)
 
+    --Contract rope if condition set
     if contractingRope and readyToDraw then
         objects.rope.length = objects.rope.length - 3
         objects.rope.joint = love.physics.newRopeJoint(objects.ball.body, objects.rope.body, objects.ball.body:getX(), objects.ball.body:getY(), objects.rope.body:getX(), objects.rope.body:getY(), objects.rope.length)
     end
+
+    if love.mouse.isDown(1) then
+        contractingRope = true
+    else
+        contractingRope = false
+    end
+    --Cut rope if mouse has crossed rope
+    if readyToDraw and not objects.rope.body == nil then
+        xNewMouse, yNewMouse = love.mouse.getPosition()
+        cross, xCross, yCross = segmentIntersection(objects.ball.body:getX(), objects.ball.body:getY(), objects.rope.body:getX(), objects.rope.body:getY(), xPriorMouse, yPriorMouse, xNewMouse, yNewMouse)
+        if cross then
+            removeRope()
+        end
+    end
+
+    --Update mouse positions
+    xPriorMouse = xNewMouse
+    yPriorMouse = yNewMouse
+
 
 end
 
@@ -50,8 +70,6 @@ function love.mousepressed(x,y,button, istouch, presses)
     elseif button == 1 then
         if not readyToDraw then
             shootRope()
-        else
-            contractingRope = true
         end
     end
 end
@@ -62,6 +80,36 @@ function love.mousereleased(x,y,button)
     end
 end
 
+function segmentIntersection(s1x1, s1y1, s1x2, s1y2, s2x1, s2y1, s2x2, s2y2)
+	-- compensate for horizontal & verticle lines
+	local eps = 0.0001
+	if s1x1 == s1x2 then s1x2 = s1x2 + eps end
+	if s1y1 == s1y2 then s1y2 = s1y2 + eps end
+	if s2x1 == s2x2 then s2x2 = s2x2 + eps end
+	if s2y1 == s2y2 then s2y2 = s2y2 + eps end
+	-- fit linear equation to line segments
+	local slope1 = ( s1y2 - s1y1 ) / ( s1x2 - s1x1 )	-- line1 slope
+	local slope2 = ( s2y2 - s2y1 ) / ( s2x2 - s2x1 )	-- line2 slope
+	local int1	 = s1y1 - slope1 * s1x1		-- line1 intercept
+	local int2	 = s2y1 - slope2 * s2x1		-- line2 intercept
+	-- intercept coordinates
+	if slope1 == slope2 then slope1 = slope1 + eps end	-- avoid div by zero
+	local x = ( int2 - int1 ) / ( slope1 - slope2 )
+	local y = slope2 * x + int2
+	-- check order of points
+	if s1x1 > s1x2 then s1x1, s1x2 = s1x2, s1x1 end
+	if s1y1 > s1y2 then s1y1, s1y2 = s1y2, s1y1 end
+	if s2x1 > s2x2 then s2x1, s2x2 = s2x2, s2x1 end
+	if s2y1 > s2y2 then s2y1, s2y2 = s2y2, s2y1 end
+	-- check if contact point is on both line segments
+	if  x >= s1x1 and x <= s1x2 and x >= s2x1 and x <= s2x2 and
+		y >= s1y1 and y <= s1y2 and y >= s2y1 and y <= s2y2 then
+		return true, x, y
+		else
+		return false, x, y
+		end
+	end
+
 
 function initialiseGlobalVariables()
     love.physics.setMeter(64)
@@ -69,5 +117,6 @@ function initialiseGlobalVariables()
     objects = {}
     readyToDraw = false
     contractingRope = false
-
+    xPriorMouse = 0
+    yPriorMouse = 0
 end
